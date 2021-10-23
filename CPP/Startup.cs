d6f17f2d7e7961d9cfb1149d42d227b50c00a1d3
Leriatod <artem.viznuk@gmail.com>
2021-10-23 23:04:23 +0300

    Project structure - moved C# project to a seperate folder

--- CPP/Startup.cs
+++ CPP/Startup.cs
@@ -28,7 +28,7 @@ namespace CPP
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
-                configuration.RootPath = "ClientApp/dist";
+                configuration.RootPath = "../ClientApp/dist";
             });
         }
 
@@ -67,7 +67,7 @@ namespace CPP
                 // To learn more about options for serving an Angular SPA from ASP.NET Core,
                 // see https://go.microsoft.com/fwlink/?linkid=864501
 
-                spa.Options.SourcePath = "ClientApp";
+                spa.Options.SourcePath = "../ClientApp";
 
                 if (env.IsDevelopment())
                 {
