d2f0cd6bbd425c6e9271cd1e90b896b2685e63ee
Leriatod <artem.viznuk@gmail.com>
2021-11-16 14:38:00 +0200

    Car form - replaced dropdowns with typeahead for categorical features

--- CPP.Client/src/app/app.module.ts
+++ CPP.Client/src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
+import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { CustomFormsModule } from "ng2-validation";
 
 import { AppComponent } from "./app.component";
-import { NavbarComponent } from "./navbar/navbar.component";
 import { CarFormComponent } from "./car-form/car-form.component";
-import { CustomFormsModule } from "ng2-validation";
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent, CarFormComponent],
@@ -15,6 +16,7 @@ import { CustomFormsModule } from "ng2-validation";
     BrowserModule.withServerTransition({ appId: "ng-cli-universal" }),
     HttpClientModule,
     FormsModule,
+    NgbModule,
     CustomFormsModule,
     RouterModule.forRoot([{ path: "", component: CarFormComponent }]),
   ],
--- CPP.Client/src/app/car-form/car-form.component.ts
+++ CPP.Client/src/app/car-form/car-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { debounceTime, distinctUntilChanged, map } from "rxjs/operators";
+
 import { CarService } from "../car.service";
 import { Car } from "../models/car";
 import { CarFeatureCategories } from "../models/car-feature-categories";
@@ -9,6 +12,15 @@ import { CarFeatureCategories } from "../models/car-feature-categories";
   styleUrls: ["./car-form.component.css"],
 })
 export class CarFormComponent implements OnInit {
+  isLoading = true;
+
+  searchProducer$: (text$: Observable<string>) => Observable<string[]>;
+  searchModel$: (text$: Observable<string>) => Observable<string[]>;
+  searchBody$: (text$: Observable<string>) => Observable<string[]>;
+  searchDrive$: (text$: Observable<string>) => Observable<string[]>;
+  searchTransmission$: (text$: Observable<string>) => Observable<string[]>;
+  searchFuel$: (text$: Observable<string>) => Observable<string[]>;
+
   featureCategories: CarFeatureCategories = {
     producers: [],
     models: [],
@@ -38,17 +50,45 @@ export class CarFormComponent implements OnInit {
   ngOnInit() {
     this.carService.getCarFeatureCategories().subscribe((featureCategories) => {
       this.featureCategories = featureCategories;
-    });
-  }
+      this.searchProducer$ = this.createSearchPipe(featureCategories.producers);
+      this.searchModel$ = this.createSearchPipe(featureCategories.models);
+      this.searchBody$ = this.createSearchPipe(featureCategories.bodies);
+      this.searchDrive$ = this.createSearchPipe(featureCategories.drives);
+      this.searchTransmission$ = this.createSearchPipe(
+        featureCategories.transmissions
+      );
+      this.searchFuel$ = this.createSearchPipe(this.featureCategories.fuels);
 
-  onProducerChange(producer: string) {
-    this.car.producer = producer;
-    this.car.model = "";
+      this.isLoading = false;
+    });
   }
 
   predictPrice() {
     this.carService.predictPriceForCar(this.car).subscribe((price) => {
       this.price = price;
     });
+  }
+
+  createSearchPipe(categories: string[]) {
+    return (text$: Observable<string>) =>
+      text$.pipe(
+        debounceTime(200),
+        distinctUntilChanged(),
+        map((template: string) =>
+          this.filterCategoriesByTemplate(categories, template).slice(0, 10)
+        )
+      );
+  }
+
+  filterCategoriesByTemplate(categories: string[], template: string) {
+    template = template.toLowerCase();
+
+    const filteredCategories = categories.filter((category) => {
+      category = category.toLowerCase();
+      const hasTemplate = category.indexOf(template) > -1;
+      return hasTemplate;
+    });
+
+    return filteredCategories;
   }
 }
